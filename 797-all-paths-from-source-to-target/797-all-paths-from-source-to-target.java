77fa8e2b089ab7ce20a72c651115bd269342492e
yashpat016 <85250036+yashpat016@users.noreply.github.com>
2022-01-12 02:18:16 -0500

    Time: 8 ms (16.91%), Space: 41.1 MB (40.51%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -2,23 +2,21 @@ class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         int target = graph.length - 1;
         List<List<Integer>> result = new ArrayList<>();
-        LinkedList<Integer> path = new LinkedList<>();
-        path.addLast(0);
-        backtrack(graph, 0, target, path, result);
+        Queue<List<Integer>> queue = new LinkedList<>();
+        List<Integer> path = new LinkedList<>();
+        queue.add(Arrays.asList(0));
+        while(!queue.isEmpty()){
+            path = queue.poll();
+            int val = path.get(path.size() - 1);
+            if(val == target){
+                result.add(path);
+            }
+            for(int neighbor: graph[val]){
+                List<Integer> list = new ArrayList<>(path);
+                list.add(neighbor);
+                queue.add(list);   
+            }
+        }
         return result;
-    }
-    
-    public void backtrack(int[][] graph, int currNode, int target, LinkedList<Integer> path, List<List<Integer>> result){
-        
-        if(currNode == target){
-            result.add(new ArrayList<Integer>(path));
-            return;
-        }
-        
-        for(int neighbor : graph[currNode]){
-            path.addLast(neighbor);
-            backtrack(graph, neighbor, target, path, result);
-            path.removeLast();
-        }
     }
 }
