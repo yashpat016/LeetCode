6476668ac00f8586375ad310b802d81c8df7e16e
yashpat016 <85250036+yashpat016@users.noreply.github.com>
2022-02-15 13:08:51 -0500

    Time: 163 ms (7.34%), Space: 68.2 MB (10.74%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.java
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -1,21 +1,28 @@
 class Solution {
     public int uniqueLetterString(String s) {
-       int result = 0;
-        int count = 0;
-        int[] lastSeen = new int[26];
-        int[] lastCount = new int[26];
-        Arrays.fill(lastSeen, -1);
-        
-        for (int i = 0; i < s.length(); i++) {
+        int[] left = new int[s.length()];
+        int[] right = new int[s.length()];
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i< s.length(); i++){
             char c = s.charAt(i);
-            int idx = c - 'A';
-            int currCnt = i - lastSeen[idx];
-            count = count - lastCount[idx] + currCnt;
-            lastSeen[idx] = i;
-            lastCount[idx] = currCnt;
-            result += count;
+            map.putIfAbsent(c, -1);
+            left[i] = map.get(c);
+            map.put(c, i);
+        }
+        map.clear();
+        for(int i = s.length() - 1; i>=0; i--){
+            char c = s.charAt(i);
+            map.putIfAbsent(c, s.length());
+            right[i] = map.get(c);
+            map.put(c, i);
         }
         
-        return result;
+        int res = 0;
+        for(int i =0; i< s.length(); i++){
+            int numLeft = i - left[i];
+            int numRight = right[i] - i;
+            res += numLeft * numRight;
+        }
+        return res;
     }
 }
