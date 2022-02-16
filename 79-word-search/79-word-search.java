e66cf42483c104801b17df6d6a8d93ba7e0bffca
yashpat016 <85250036+yashpat016@users.noreply.github.com>
2022-02-16 18:46:55 -0500

    Time: 232 ms (26.58%), Space: 118.1 MB (9.29%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,6 +1,5 @@
 class Solution {
-    
-    private char[][] board;
+   private char[][] board;
     private int rows;
     private int cols;
     
@@ -9,9 +8,9 @@ class Solution {
         this.rows = board.length;
         this.cols = board[0].length;
         
-        for(int row = 0; row < this.rows; row++){
-            for(int col = 0; col < this.cols; col++){
-                if(this.backtrack(row, col, word, 0)){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(this.backtrack(i, j, word, 0)){
                     return true;
                 }
             }
@@ -19,25 +18,27 @@ class Solution {
         return false;
     }
     
-    protected boolean backtrack(int row, int col, String word, int index){
+    public boolean backtrack(int row, int col, String word, int index){
         if(index >= word.length()){
             return true;
         }
-        if(row < 0 || row == this.rows || col < 0 || col == this.cols || this.board[row][col] != word.charAt(index)){
+        
+        if(row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != word.charAt(index)){
             return false;
         }
         
-        this.board[row][col] = '#';
+        board[row][col] = '#';
+        int[] dirRow = {0, 1, 0, -1};
+        int[] dirCol = {1, 0, -1, 0};
         
-        int[] rowOffsets = {0, 1, 0, -1};
-        int[] colOffsets = {1, 0, -1, 0};
-        
-        for(int i = 0; i < 4; i++){
-            if(this.backtrack(row + rowOffsets[i], col + colOffsets[i], word, index + 1)){
+        for(int i = 0; i < dirRow.length; i++){
+            if(backtrack(row + dirRow[i], col + dirCol[i], word, index + 1)){
                 return true;
             }
         }
-        this.board[row][col] = word.charAt(index);
+        
+        board[row][col] = word.charAt(index);
+            
         return false;
     }
 }
