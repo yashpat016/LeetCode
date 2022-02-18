776d23503764cf68499c97506edf4fac56dfe54f
yashpat016 <85250036+yashpat016@users.noreply.github.com>
2022-02-18 14:29:32 -0500

    Time: 15 ms (91.09%), Space: 45.3 MB (84.32%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,5 +1,25 @@
 class Solution {
-    int lower_bound(String[] products, int start, String word){
+    
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Arrays.sort(products);
+        List<List<String>> result = new ArrayList<>();
+        int start = 0, searchStart = 0, n = products.length;
+        String prefix = new String();
+        for(char c: searchWord.toCharArray()){
+            prefix += c;
+            start = ll(products, searchStart, prefix);
+            result.add(new ArrayList<>());
+            for(int i = start; i < Math.min(start + 3, n); i++){
+                if(products[i].length() < prefix.length() || !products[i].substring(0, prefix.length()).equals(prefix)){
+                    break;
+                }
+                result.get(result.size() - 1).add(products[i]);
+            }
+            searchStart = start;
+        }
+        return result;
+    }
+    int ll(String[] products, int start, String word){
         int i = start, j = products.length, mid;
         while(i < j){
             mid = (i + j) /2;
@@ -10,27 +30,5 @@ class Solution {
             }
         }
         return i;
-    }
-    
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        Arrays.sort(products);
-        List<List<String>> result = new ArrayList<>();
-        int start = 0, bsStart = 0, n = products.length;
-        String prefix = new String();
-        for(char c: searchWord.toCharArray()){
-            prefix += c;
-            
-            start = lower_bound(products, bsStart, prefix);
-            
-            result.add(new ArrayList<>());
-            for(int i = start; i < Math.min(start + 3, n); i++){
-                if(products[i].length() < prefix.length() || !products[i].substring(0, prefix.length()).equals(prefix)){
-                    break;
-                }
-                result.get(result.size() - 1).add(products[i]);
-            }
-            bsStart = start;
-        }
-        return result;
     }
 }
